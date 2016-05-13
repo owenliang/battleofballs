fb74a50dbb775042a840d145d8e2020b9f698bac
wangliqin01 <wangliqin01@baidu.com>
2016-05-13 19:14:50 +0800

    conf

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;port=3306;dbname=battleofballs',
+            'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=battleofballs',
             'username' => 'root',
             'password' => 'root',
             'charset' => 'utf8',
